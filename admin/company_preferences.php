34a81bf45301d1599b6b30e80cdc224c4449cdca
MB <anoopmb12@gmail.com>
2017-07-08 13:10:35 +0530

    Added VAT Number for Company

--- admin/company_preferences.php
+++ admin/company_preferences.php
@@ -173,6 +173,7 @@ email_row_ex(_("BCC Address for all outgoing mails:"), 'bcc_email', 50, 55);
 
 text_row_ex(_("Official Company Number:"), 'coy_no', 25);
 text_row_ex(_("GSTNo:"), 'gst_no', 25);
+text_row_ex(_("VAT No:"), 'vat_no', 25);
 
 currencies_list_row(_("Home Currency:"), 'curr_default', $_POST['curr_default']);
 fiscalyears_list_row(_("Fiscal Year:"), 'f_year', $_POST['f_year']);
